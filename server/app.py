6a36bd15192acfb635d955f971e2bd0cb8d9015f
baruh-d <barudaniel6@gmail.com>
2024-04-05 15:45:26 +0300

    done

--- server/app.py
+++ server/app.py
@@ -21,7 +21,29 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def earthquakes_by_id(id):
+    earthquakes = Earthquake.query.filter(Earthquake.id==id).all()
+    if earthquakes:
+        eq = earthquakes[0]
+        body =  eq.to_dict()
+        status = 200
+    else:
+        body = {'message': f'Earthquake {id} not found.'}
+        status = 404
+            
+    return make_response(body, status)
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def  earthquakes_by_magnitude(magnitude):
+    earthquakes = []
+    for eq in Earthquake.query.filter(Earthquake.magnitude>=magnitude).all():
+        earthquakes.append(eq.to_dict())
+    body = {'count': len(earthquakes),
+            'quakes': earthquakes}
+    
+    return make_response(body, 200)
 
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(port=5000, debug=True)
